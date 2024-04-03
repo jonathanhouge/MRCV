334dc9e72891a492dd635281e39c03dceb2f9e38
jonathanh <jonathanhouge@email.arizona.edu>
2024-04-03 12:13:31 -0700

    forgot to remove csv path

--- listscraper/cli.py
+++ listscraper/cli.py
@@ -36,7 +36,7 @@ def cli_arguments():
         type=str,
         help="set the path for the output CSV(s). Default output is a folder called 'scraper_outputs'.",
         required=False,
-        default="scraper_outputs/csv",
+        default="scraper_outputs",
     )
 
     parser.add_argument(
